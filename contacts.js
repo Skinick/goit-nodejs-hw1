bebe862a50f2ca218b13125930fd92f5cc51c90f
Skinick <107771280+Skinick@users.noreply.github.com>
2023-09-24 22:03:30 +0300

    fix console.table

--- contacts.js
+++ contacts.js
@@ -3,6 +3,7 @@ import path from "path";
 import { nanoid } from "nanoid";
 
 const contactsPath = path.resolve("db", "contacts.json");
+
 const updateContacts = (contacts) =>
   fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
     switch (action) {
       case "list":
         const contactsList = await contactsService.listContacts();
-        return console.log(contactsList);
+        return console.table(contactsList);
 
       case "get":
         const oneContact = await contactsService.getContactById(id);
@@ -30,6 +30,7 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
           phone,
         });
         return console.log(newContact);
+
       case "remove":
         const removeContact = await contactsService.removeContact(id);
         return console.log(removeContact);
